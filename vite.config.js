d24afa88095ddb4422e6016994fbbda4758b8248
Alejandro Estringana Ruiz <estringana@gmail.com>
2025-11-30 21:08:38 +0100

    Make app PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/impostor-game/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: false,
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      }
+    })
+  ],
 })
